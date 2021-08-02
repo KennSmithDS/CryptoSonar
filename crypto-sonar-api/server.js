b9829abb50e1e0ac1449b31529242ed66f9d17d4
CryptoKrakenai <kennsmith@outlook.com>
2021-08-02 11:08:13 -0700

    committing typedefs and resolvers template

--- crypto-sonar-api/server.js
+++ crypto-sonar-api/server.js
@@ -1,16 +1,25 @@
 require('dotenv').config({path: '.env'});
 const express = require('express');
 const mongoose = require('mongoose');
+const { ApolloServer, gql } = require('apollo-server-express');
+import { resolvers } from './resolvers';
+import { typeDefs } from './typeDefs';
+   
+const startServer = async () => {
 
-const server = async () => {
-    
     const app = express();
-    const port = process.env.API_SERVER_PORT;
+    const port = process.env.API_SERVER_PORT || 3000;
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+
+    server.applyMiddleware({ app });
 
     app.listen({port}, () => {
         console.log(`Server connected and listening on ${port}`);
-    })
+    });
+};
 
-}
-
-server();
+startServer();
---
+++ crypto-sonar-api/typeDefs.js
@@ -0,0 +1,8 @@
+const { gql } = require('apollo-server-express');
+
+
+export const typeDefs = gql`
+type Query {
+    helloWorld: String!
+}
+`;
