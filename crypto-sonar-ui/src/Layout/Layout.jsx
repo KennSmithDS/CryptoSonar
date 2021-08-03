81b58cf3f8dfa8e13e36d0e0a6d7af7d0cb411d3
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-02 17:29:49 -0700

    initial layout of page applied

---
+++ crypto-sonar-ui/src/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import {  Container, Row, Col, Button } from 'react-bootstrap';
+import './Layout.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Layout = () => {
+  return (
+    <div class="wrapper">
+      <div class="nav-bar">Nav-bar area</div>
+      <div class="tracked-wallets-panel">tracked-wallets-panel area</div>
+      <div class="alerts-panel">alerts-panel</div>
+      <div class="resources-panel">resources-panel</div>
+      <div class="footer">footer</div>
+    </div>
+  
+
+
+  )
+}
+
+export default Layout;
