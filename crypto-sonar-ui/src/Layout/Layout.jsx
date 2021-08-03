f6ef976b8ac9dacb7fc14cf2570f8712ec4ebbaf
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-02 23:31:56 -0700

    Changed div containers to react-bootstrap containers in Layout

--- crypto-sonar-ui/src/Layout/Layout.jsx
+++ crypto-sonar-ui/src/Layout/Layout.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
-// import {  Container, Row, Col, Button } from 'react-bootstrap';
-import './Layout.css'
+import './Layout.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {  Container, Row, Col } from 'react-bootstrap';
+
 
 export const Layout = () => {
   return (
-    <div class="wrapper">
-      <div class="nav-bar">Nav-bar area</div>
-      <div class="tracked-wallets-panel">tracked-wallets-panel area</div>
-      <div class="alerts-panel">alerts-panel</div>
-      <div class="resources-panel">resources-panel</div>
-      <div class="footer">footer</div>
-    </div>
-  
-
-
+    <Container>
+      <Row>
+        <Col className="NavBar" md={12}>
+          NavBar
+        </Col>
+      </Row>
+      <Row>
+        <Col md={3}>
+          <Row>
+            <Col className="tracked-wallet" md={12}>
+              tracked-wallet
+            </Col>
+          </Row>
+        </Col>
+        <Col md={9}>
+          <Row>
+            <Col className="alerts-panel" md={12}>
+              alerts-panel
+            </Col>
+          </Row>
+          <Row>
+            <Col className="resources-panel" md={12}>
+              resources-panel
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   )
 }
-
-export default Layout;
