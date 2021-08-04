ac27f51727766c3bb1b87dc69f96a15058998255
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-03 23:20:13 -0700

    Added Tracked Wallets Panel skeleton

--- crypto-sonar-ui/src/NavBar/NavBarPanel.jsx
+++ crypto-sonar-ui/src/NavBar/NavBarPanel.jsx
@@ -1,6 +1,10 @@
 import React, { Component, useState } from 'react'
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import './NavBarPanel.css'
+import {Broadcast} from 'react-bootstrap-icons';
+
 import { AccountSettings } from '../AccountSettings'
+
 
 function NavBar() {
   const [showModal, setShowModal] = useState(false)
@@ -10,19 +14,21 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-      <Navbar.Brand>CryptoSONAR</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto"></Nav>
-        <Nav>
-          <NavDropdown title="Username" id="collasible-nav-dropdown">
-            <NavDropdown.Item onClick={openModal}>Account Settings</NavDropdown.Item>
-            <AccountSettings showModal={showModal} setShowModal={setShowModal} />
-            <NavDropdown.Divider />
-            <NavDropdown.Item  >Logout</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+        <Broadcast size={50}/>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto Brand">
+            <Navbar.Brand >CryptoSONAR</Navbar.Brand>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Username" id="collasible-nav-dropdown">
+              <NavDropdown.Item onClick={openModal}>Account Settings</NavDropdown.Item>
+              <AccountSettings showModal={showModal} setShowModal={setShowModal} />
+              <NavDropdown.Divider />
+              <NavDropdown.Item  >Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
--- crypto-sonar-ui/src/Tracked-Wallets-Panel/TrackedWalletsPanel.jsx
+++ crypto-sonar-ui/src/Tracked-Wallets-Panel/TrackedWalletsPanel.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
+import { ListGroup, Container, Button } from 'react-bootstrap'
+import { PlusCircle, DashCircle } from 'react-bootstrap-icons'
+import './TrackedWalletsPanel.css'
+
+
+const walletList = ['Wallet 1', 'Wallet 2', 'Wallet 3']
+
+
+function ListWallets() {
+  return(
+    walletList.map(wallet => (        
+      <ListGroup action defaultActiveKey={`#${wallet}`}>
+        <ListGroup.Item action variant="light">
+          {wallet}
+        </ListGroup.Item>
+      </ListGroup>
+    ))
+  )
+}
 
 export class TrackedWalletsPanel extends Component {
   render() {
     return (
-      <div>
-        Tracked Wallets Panel Here
+      <div class="wallets-list-panel">
+        <h3>Tracked Wallets Panel </h3>
+        <Container className="wallets-list-container">
+          <ListWallets />
+        </Container>
+        <Container className="add-delete-wallet">
+          <Button variant="outline-secondary"><PlusCircle size={50} /></Button>
+          <Button variant="outline-danger"><DashCircle size={50} /></Button>
+        </Container>      
       </div>
     )
   }
