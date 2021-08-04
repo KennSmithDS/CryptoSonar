d8e1e587802bd240ff81d075cbf592ab89f108e4
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-03 17:59:59 -0700

    Added Account Settings Modal

--- crypto-sonar-ui/src/NavBar/NavBarPanel.jsx
+++ crypto-sonar-ui/src/NavBar/NavBarPanel.jsx
@@ -1,21 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { AccountSettings } from '../AccountSettings'
 
 function NavBar() {
+  const [showModal, setShowModal] = useState(false)
+  const openModal = () => {
+    setShowModal(prev => !prev)
+  }
   return (
-    <Navbar >
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand >CryptoSONAR</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Nav className="mr-auto">
-          <NavDropdown title="UserName Here" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.3">Account Settings</NavDropdown.Item>
+      <Navbar.Brand>CryptoSONAR</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto"></Nav>
+        <Nav>
+          <NavDropdown title="Username" id="collasible-nav-dropdown">
+            <NavDropdown.Item onClick={openModal}>Account Settings</NavDropdown.Item>
+            <AccountSettings showModal={showModal} setShowModal={setShowModal} />
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+            <NavDropdown.Item  >Logout</NavDropdown.Item>
           </NavDropdown>
-          </Nav>
+        </Nav>
+      </Navbar.Collapse>
       </Container>
-    </Navbar> 
+    </Navbar>
   );
 }
 
