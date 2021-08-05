260ed89114fce5b2ab6cc9104e07069fd3a7e6ab
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-04 19:04:09 -0700

    Fixed cache bug

--- crypto-sonar-ui/src/App.js
+++ crypto-sonar-ui/src/App.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ApolloClient, ApolloProvider } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import { Layout } from './Layout/Layout'
+require('dotenv').config({path: '../.env'});
 
 const client = new ApolloClient({
-  uri: process.env.API_ENDPOINT
+  uri: process.env.REACT_APP_API_ENDPOINT,
+  cache: new InMemoryCache()
 });
 
 function App() {
