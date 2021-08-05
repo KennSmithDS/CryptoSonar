f7a4a67d276f08050fe09b5ec7916cd00d42be5e
CryptoKrakenai <kennsmith@outlook.com>
2021-08-05 08:55:51 -0700

    completed query definitions

--- crypto-sonar-ui/src/queries/graphqlQueries.js
+++ crypto-sonar-ui/src/queries/graphqlQueries.js
@@ -21,13 +21,54 @@ const getWallets = gql`
 `;
 
 const getUserWallets = gql`
-    query{
-        user(id: ${id}) {
+    query GetUserWallets($id: ID) {
+        user(id: $id) {
         id
         wallets {
             alias
             address
-        }
+            }
         }
     }
 `;
+
+const addWallet = gql`
+    mutation ($alias: String!, $address: String!, $userId: ID!) {
+        addWallet(alias: $alias, address: $address, userId: $userId) {
+            id
+            alias
+            address
+            userId
+      }
+    }
+`;
+
+const removeWallet = gql`
+    mutation ($id: ID!) {
+        removeWallet(id: $id) {
+            id
+            alias
+        }
+    }
+`;
+
+const addUser = gql`
+    mutation ($userName: String!, $userPassword: String!) {
+        addUser(userName: $userName, userPassword: $userPassword) {
+            id
+            userName
+            userPassword
+        }
+    }
+`;
+
+const removeUser = gql`
+    mutation ($id: ID!) {
+        removeUser(id: $id) {
+            id
+            userName
+        }
+    }
+`;
+
+export { getUsers, getWallets, getUserWallets, addWallet, removeWallet, addUser, removeUser };
