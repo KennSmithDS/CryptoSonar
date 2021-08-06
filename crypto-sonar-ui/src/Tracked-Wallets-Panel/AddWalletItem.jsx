e4e1eeda15fa042b6968ff1e8bb2165d16cdc88b
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-06 10:43:45 -0700

    refactored out ListWallets

--- crypto-sonar-ui/src/Tracked-Wallets-Panel/AddWalletItem.jsx
+++ crypto-sonar-ui/src/Tracked-Wallets-Panel/AddWalletItem.jsx
@@ -46,25 +46,20 @@ export const  AddWalletItem = (props) => {
             <Modal.Title>Add Wallet</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Container>
-          
+            <Container>          
               <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formWalletAddress">
                   <Form.Label>Wallet Address:</Form.Label>
                   <Form.Control required type="string" placeholder="Wallet Address" name="walletAddress" onChange={handleInputChange} value={inputs.walletAddress || ''}/>
-
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formAddressAlias">
                   <Form.Label>Address Alias:</Form.Label>
                   <Form.Control required type="string" placeholder="Address Alias" name="walletAlias" onChange={handleInputChange} value={inputs.walletAlias || ''}/>
                 </Form.Group>
-
                 <Button variant="primary" type="submit">
                   Submit
                 </Button>
               </Form>
-
             </Container>
           </Modal.Body>
           <Modal.Footer>
