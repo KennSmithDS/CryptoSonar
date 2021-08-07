7649fb40c6146a9e5fce50fb47442285167ddb50
inocejoff <inocencio.jo@husky.neu.edu>
2021-08-06 17:59:13 -0700

    implemented listener for selected wallets

--- ui/src/Tracked-Wallets-Panel/ListWallets.jsx
+++ ui/src/Tracked-Wallets-Panel/ListWallets.jsx
@@ -22,17 +22,18 @@ export function ListWallets(props) {
     }).then((response) => response.json())
       .then((data) => setWalletList(data.data.user.wallets));
   });
-    
-  return(
-    walletList.map(wallet => (        
-      <ListGroup defaultActiveKey={`#${wallet.address}`} key={`#${wallet.address}`}>
-        <ListGroup.Item action variant="light">
-          {wallet.alias}
-        </ListGroup.Item>
-      </ListGroup>
-    ))
+
+  const ItemizedWallets = walletList.map((wallet, i)  => (        
+    <ListGroup.Item action onClick={()=>console.log(wallet.address)} variant="light" key={i}>
+      {wallet.alias}
+    </ListGroup.Item>
+  ))
+
+  return(            
+      <ListGroup defaultActiveKey={"wallet-list-active-key"} key={"wallet-list-key"}>
+          {ItemizedWallets}     
+      </ListGroup>    
   )
 }
-
 
 export default ListWallets
